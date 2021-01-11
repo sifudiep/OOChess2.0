8ce06a1233a023f78d8078370d674df13469ce28
sifudiep <code.sifudiep@gmail.com>
2021-01-11 17:44:01 +0100

    Added Light and darkSquare Added Castling

--- OOChess2.0/Board.cpp
+++ OOChess2.0/Board.cpp
@@ -18,10 +18,10 @@ Board::Board() {
 }
 
 void Board::gameInput() {
+start:
+
 	std::string turnText = (turn == Owner::Blue) ? "BLUE" : "RED";
 	int textColor = turn == Owner::Blue ? 9 : 12;
-
-	start:
 
 	std::string input;
 
@@ -34,10 +34,36 @@ void Board::gameInput() {
 	std::cin >> input;
 
 	char symbol = input[0];
-	Coordinate start(std::tolower(input[1]) - 97, (int)input[2] - 49);
-	Coordinate destination(std::tolower(input[3]) - 97, (int)input[4] - 49);
+
+	if (input.size() == 2) {
+		if (input[0] == 'C') {
+			if (input[1] == 'K') {
+				if (castle(turn, true)) {
+					turn = turn == Owner::Blue ? Owner::Red : Owner::Blue;
+					goto start;
+				}
+			}
+
+			if (input[1] == 'Q') {
+				if (castle(turn, false)) {
+					turn = turn == Owner::Blue ? Owner::Red : Owner::Blue;
+					goto start;
+				}
+			}
+			
+			std::cout << "Castling failed...";
+			goto start;
+		}
+		std::cout << "Castling failed...";
+		goto start;
+	}
+
+
 
 	if (input.size() == 5) {
+		Coordinate start(std::tolower(input[1]) - 97, (int)input[2] - 49);
+		Coordinate destination(std::tolower(input[3]) - 97, (int)input[4] - 49);
+		
 		if (std::isalpha(input[0]) && std::isalpha(input[1]) && std::isalpha(input[3]) ) {
 			if (start.y >= 0 && start.y < 8 && destination.y >= 0 && destination.y < 8) {
 				if (!kingBecomesChecked(symbol, start, destination)) {
@@ -68,6 +94,103 @@ void Board::gameInput() {
 
 }
 
+int Board::findPiece(char symbol, int x, int y) {
+	for (size_t i = 0; i < allPieces.size(); i++) {
+		if (allPieces[i]->symbol == symbol) {
+			if (allPieces[i]->coordinate.x == x && allPieces[i]->coordinate.y == y) {
+				return i;
+			}
+		}
+	}
+
+	return 100;
+}
+
+int Board::kingIsUnderCheck(Owner owner) {
+	Piece* king = allPieces[getKingIndex(owner)];
+	
+	for (size_t i = 0; i < allPieces.size(); i++) {
+		for (size_t j = 0; j < allPieces[i]->possibleCoordinates.size(); j++) {
+			if (allPieces[i]->owner != owner) {
+				if (allPieces[i]->possibleCoordinates[j].x == king->coordinate.x && allPieces[i]->possibleCoordinates[j].y == king->coordinate.y) {
+					return true;
+				}
+			}
+		}
+	}
+
+	return false;
+}
+
+
+bool Board::castle(Owner owner, bool kingSide) {
+	Piece* king = allPieces[getKingIndex(owner)];
+	if (king->firstMove && kingIsUnderCheck(owner) == false) {
+		Coordinate kingStartCoordinate = king->coordinate;
+		
+		int row = owner == Owner::Blue ? 0 : 7;
+		int column = kingSide == true ? 7 : 0;
+		
+		int rookIndex = findPiece('R', column, row);
+		if (rookIndex == 100) return false;
+		Piece* rook = allPieces[rookIndex];
+
+		if (!rook->firstMove) return false;
+
+		if (kingSide) {
+			for (int i = 1; i < 3; i++) {
+				if (board[kingStartCoordinate.y][kingStartCoordinate.x + i].owner == Owner::None) {
+					if (kingBecomesChecked('K', king->coordinate, Coordinate(kingStartCoordinate.x + i, kingStartCoordinate.y))) {
+						king->coordinate = kingStartCoordinate;
+						updateAllPossibleCoordinates();
+						return false;
+					}
+				}
+				else {
+					king->coordinate = kingStartCoordinate;
+					updateAllPossibleCoordinates();
+					return false;
+				}
+			}
+
+			rook->coordinate = Coordinate(kingStartCoordinate.x + 1, kingStartCoordinate.y);
+			king->coordinate = Coordinate(kingStartCoordinate.x + 2, kingStartCoordinate.y);
+			clearBoard();
+			updateAllPossibleCoordinates();
+			drawBoard();
+			
+			return true;
+		} else {
+			for (int i = 1; i < 4; i++) {
+				if (board[kingStartCoordinate.y][kingStartCoordinate.x - i].owner == Owner::None) {
+					if (kingBecomesChecked('K', king->coordinate, Coordinate(kingStartCoordinate.x - i, kingStartCoordinate.y))) {
+						king->coordinate = kingStartCoordinate;
+						king->updatePossibleCoordinates(board);
+						std::cout << "3";
+						return false;
+					}
+				}
+				else {
+					king->coordinate = kingStartCoordinate;
+					king->updatePossibleCoordinates(board);
+					std::cout << "4";
+					return false;
+				}
+			}
+
+			rook->coordinate = Coordinate(kingStartCoordinate.x - 1, kingStartCoordinate.y);
+			king->coordinate = Coordinate(kingStartCoordinate.x - 2, kingStartCoordinate.y);
+			
+			clearBoard();
+			updateAllPossibleCoordinates();
+			drawBoard();
+			return true;
+		}
+	}
+	return false;
+}
+
+
 void Board::captureSquare(Coordinate destination) {
 	for (size_t i = 0; i < allPieces.size(); i++) {
 		if (allPieces[i]->coordinate.x == destination.x && allPieces[i]->coordinate.y == destination.y) {
@@ -96,7 +219,6 @@ bool Board::kingBecomesChecked(char symbol, Coordinate start, Coordinate destina
 	moveBack(symbol, destination);
 	return false;
 }
-
 void Board::moveBack(char symbol, Coordinate coordinate) {
 	for (size_t i = 0; i < allPieces.size(); i++) {
 		if (allPieces[i]->symbol == symbol && allPieces[i]->coordinate.x == coordinate.x && allPieces[i]->coordinate.y == coordinate.y) {
@@ -106,29 +228,23 @@ void Board::moveBack(char symbol, Coordinate coordinate) {
 		}
 	}
 }
-
 int Board::getKingIndex(Owner owner) {
 	if (owner == Owner::Blue) {
 		return allPieces.size() - 2;
 	} else if (owner == Owner::Red) {
 		return allPieces.size() - 1;
 	}
-}
 
+	return ERROR;
+}
 bool Board::movePiece(char symbol, Coordinate start, Coordinate destination, bool simulate) {	
 	for (size_t i = 0; i < allPieces.size(); i++) {
 		if (allPieces[i]->owner == turn) {
-			std::cout << "1";
 			if (allPieces[i]->symbol == symbol) {
-				std::cout << "2";
-
 				if (allPieces[i]->coordinate.x == start.x && allPieces[i]->coordinate.y == start.y) {
-					std::cout << "3";
-
 					for (size_t j = 0; j < allPieces[i]->possibleCoordinates.size(); j++) {
 						if (allPieces[i]->possibleCoordinates[j].x == destination.x 
 							&& allPieces[i]->possibleCoordinates[j].y == destination.y) {
-							std::cout << "4";
 
 							if (simulate == false) {
 								captureSquare(destination);
@@ -166,18 +282,42 @@ void Board::updateAllPossibleCoordinates() {
 	}
 }
 void Board::drawBoard() {
+	bool lightSquare = true;
 	for (int i = 7; i >= 0; i--) {
 		std::cout << '|' << i + 1 << "| ";
 		for (int j = 0; j < 8; j++) {
+			if (lightSquare) {
+				SetConsoleTextAttribute(h_console, 7);
+			} else {
+				SetConsoleTextAttribute(h_console, 2);
+			}
+
+			std::cout << '[';
+
+			
 			if (board[i][j].owner == Owner::Blue) {
 				SetConsoleTextAttribute(h_console, 9);
-			}
-
-			if (board[i][j].owner == Owner::Red) {
+			} else if (board[i][j].owner == Owner::Red) {
 				SetConsoleTextAttribute(h_console, 12);
 			}
+			
+			std::cout << board[i][j].symbol;
+			
+			if (lightSquare) {
+				SetConsoleTextAttribute(h_console, 7);
+				if (j != 7) {
+					lightSquare = lightSquare == true ? false : true;
+				}
+			}
+			else {
+				SetConsoleTextAttribute(h_console, 2);
+				if (j != 7) {
+					lightSquare = lightSquare == true ? false : true;
+				}
+			}
 
-			std::cout << '[' << board[i][j].symbol << ']';
+			std::cout << ']';
+
 			SetConsoleTextAttribute(h_console, 7);
 		}
 		std::cout << std::endl;
@@ -205,10 +345,10 @@ void Board::resetBoardPieces() {
 	allPieces.clear();
 
 	// Spawning Pawns
-	for (int i = 0; i < 8; i++) {
-		allPieces.emplace_back(new Pawn(Coordinate(i, 1), Owner::Blue, 'P'));
-		allPieces.emplace_back(new Pawn(Coordinate(i, 6), Owner::Red, 'P'));
-	}
+	//for (int i = 0; i < 8; i++) {
+	//	allPieces.emplace_back(new Pawn(Coordinate(i, 1), Owner::Blue, 'P'));
+	//	allPieces.emplace_back(new Pawn(Coordinate(i, 6), Owner::Red, 'P'));
+	//}
 
 	// Spawning Rooks
 	allPieces.emplace_back(new Rook(Coordinate(0, 0), Owner::Blue, 'R'));
@@ -217,20 +357,20 @@ void Board::resetBoardPieces() {
 	allPieces.emplace_back(new Rook(Coordinate(7, 7), Owner::Red, 'R'));
 
 	//// Spawning Knights
-	allPieces.emplace_back(new Knight(Coordinate(1, 0), Owner::Blue, 'N'));
-	allPieces.emplace_back(new Knight(Coordinate(6, 0), Owner::Blue, 'N'));
-	allPieces.emplace_back(new Knight(Coordinate(1, 7), Owner::Red, 'N'));
-	allPieces.emplace_back(new Knight(Coordinate(6, 7), Owner::Red, 'N'));
+	//allPieces.emplace_back(new Knight(Coordinate(1, 0), Owner::Blue, 'N'));
+	//allPieces.emplace_back(new Knight(Coordinate(6, 0), Owner::Blue, 'N'));
+	//allPieces.emplace_back(new Knight(Coordinate(1, 7), Owner::Red, 'N'));
+	//allPieces.emplace_back(new Knight(Coordinate(6, 7), Owner::Red, 'N'));
 
 	// Spawning Bishops
-	allPieces.emplace_back(new Bishop(Coordinate(2, 0), Owner::Blue, 'B'));
-	allPieces.emplace_back(new Bishop(Coordinate(5, 0), Owner::Blue, 'B'));
-	allPieces.emplace_back(new Bishop(Coordinate(2, 7), Owner::Red, 'B'));
-	allPieces.emplace_back(new Bishop(Coordinate(5, 7), Owner::Red, 'B'));
+	//allPieces.emplace_back(new Bishop(Coordinate(2, 0), Owner::Blue, 'B'));
+	//allPieces.emplace_back(new Bishop(Coordinate(5, 0), Owner::Blue, 'B'));
+	//allPieces.emplace_back(new Bishop(Coordinate(2, 7), Owner::Red, 'B'));
+	//allPieces.emplace_back(new Bishop(Coordinate(5, 7), Owner::Red, 'B'));
 
 	//// Spawning Queens
-	allPieces.emplace_back(new Queen(Coordinate(3, 0), Owner::Blue, 'Q'));
-	allPieces.emplace_back(new Queen(Coordinate(3, 7), Owner::Red, 'Q'));
+	//allPieces.emplace_back(new Queen(Coordinate(3, 0), Owner::Blue, 'Q'));
+	//allPieces.emplace_back(new Queen(Coordinate(3, 7), Owner::Red, 'Q'));
 
 	//// Spawning Kings
 	allPieces.emplace_back(new King(Coordinate(4, 0), Owner::Blue, 'K'));
--- OOChess2.0/Board.h
+++ OOChess2.0/Board.h
@@ -4,6 +4,11 @@
 #include "Move.h"
 #include "Piece.h"
 #include <windows.h>
+#include <conio.h>
+#include <stdio.h>
+
+//TODO Castling
+//TODO En Passant
 
 
 class Board
@@ -22,7 +27,10 @@ private:
 	bool kingBecomesChecked(char symbol, Coordinate start, Coordinate destination);
 	void captureSquare(Coordinate destination);
 	void moveBack(char symbol, Coordinate coordinate);
+	bool castle(Owner owner, bool kingSide);
 
+	int kingIsUnderCheck(Owner owner);
+	int findPiece(char symbol, int x, int y);
 	int getKingIndex(Owner owner);
 	void updateAllPossibleCoordinates();
 	void syncAllPiecesToBoard();
